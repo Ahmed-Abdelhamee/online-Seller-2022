381d713e934df9729e636fe7c5645640cb7543cd
Ahmed-Abdelhameed <medoabdelhameed99.yahoo.com>
2023-07-11 16:50:51 +0300

    frist commit

--- index.js
+++ index.js
@@ -15,65 +15,99 @@ navLinks.forEach(function (ele){
 
 // ------------------------- producs -------------------------
 let menProductList=[
-    {img:"./onlineSeller Menpage/menClothes0.jpg",details:"Formal Suit",stars:5},
-    {img:"./onlineSeller Menpage/menClothes8.jpg",details:"White Shirt",stars:3.5},
-    {img:"./onlineSeller Menpage/menClothes7.jpg",details:"Te-Shirt",stars:3},
-    {img:"./onlineSeller Menpage/menshoes4.jpg",details:"blue shoes",stars:2.5},
-    {img:"./onlineSeller Menpage/menClothes11.jpg",details:"package fashion",stars:4},
-    {img:"./onlineSeller Menpage/menClothes.jpg",details:"smart package fashion",stars:4},
-    {img:"./onlineSeller Menpage/bag.jpg",details:"smart package fashion",stars:4.5},
-    {img:"./onlineSeller Menpage/menshoes6.jpg",details:"smart package fashion",stars:4},
+    {img:"./onlineSeller Menpage/menClothes0.jpg",setNew:true,NewDivStyle:"circle",details:"Formal Suit",stars:5},
+    {img:"./onlineSeller Menpage/menClothes8.jpg",setNew:true,NewDivStyle:"circle",details:"White Shirt",stars:3.5},
+    {img:"./onlineSeller Menpage/menClothes7.jpg",setNew:true,NewDivStyle:"circle",details:"Te-Shirt",stars:3},
+    {img:"./onlineSeller Menpage/menshoes4.jpg",setNew:true,NewDivStyle:"circle",details:"blue shoes",stars:2.5},
+    {img:"./onlineSeller Menpage/menClothes11.jpg",setNew:true,NewDivStyle:"circle",details:"package fashion",stars:4},
+    {img:"./onlineSeller Menpage/menClothes.jpg",setNew:true,NewDivStyle:"circle",details:"smart package fashion",stars:4},
+    {img:"./onlineSeller Menpage/bag.jpg",setNew:true,NewDivStyle:"circle",details:"smart package fashion",stars:4.5},
+    {img:"./onlineSeller Menpage/menshoes6.jpg",setNew:true,NewDivStyle:"circle",details:"smart package fashion",stars:4},
 ]
 let womenProductList=[
-    {img:"./veiled-fashion/icons-photos/dress6.jpg",details:"pink dress fashion",stars:3},
-    {img:"./veiled-fashion/icons-photos/dress7.jpg",details:"green dress fashion",stars:4.5},
-    {img:"./veiled-fashion/icons-photos/dress8.jpg",details:"white dress fashion",stars:3},
-    {img:"./veiled-fashion/icons-photos/femaleshoes2.jpg",details:"gold shoes fashion",stars:4},
-    {img:"./veiled-fashion/icons-photos/femaleshoes3.jpg",details:"silver shoes fashion",stars:2.5},
-    {img:"./veiled-fashion/icons-photos/dress10.jpg",details:"sliver dress fashion",stars:4},
+    {img:"./veiled-fashion/icons-photos/dress6.jpg",setNew:true,NewDivStyle:"star",details:"pink dress fashion",stars:3},
+    {img:"./veiled-fashion/icons-photos/dress7.jpg",setNew:true,NewDivStyle:"star",details:"green dress fashion",stars:4.5},
+    {img:"./veiled-fashion/icons-photos/dress8.jpg",setNew:true,NewDivStyle:"star",details:"white dress fashion",stars:3},
+    {img:"./veiled-fashion/icons-photos/femaleshoes2.jpg",setNew:true,NewDivStyle:"star",details:"gold shoes fashion",stars:4},
+    {img:"./veiled-fashion/icons-photos/femaleshoes3.jpg",setNew:true,NewDivStyle:"star",details:"silver shoes fashion",stars:2.5},
+    {img:"./veiled-fashion/icons-photos/dress10.jpg",setNew:true,NewDivStyle:"star",details:"sliver dress fashion",stars:4},
 ]
 let childrenProductList =[
-    {img:"./onlineSeller Childpage/photos/childDress4.jpg",details:"gril dress",stars:2},
-    {img:"./onlineSeller Childpage/photos/childSet3.jpg",details:"stagger gray black",stars:3.5},
-    {img:"./onlineSeller Childpage/photos/childSet4.jpg",details:"stagger all black",stars:7},
-    {img:"./onlineSeller Childpage/photos/childShoes3.jpg",details:"shoes army",stars:4},
-    {img:"./onlineSeller Childpage/photos/childBag4.jpg",details:"pink bag",stars:2.5},
-    {img:"./onlineSeller Childpage/photos/childShoes2.jpg",details:"red shoes",stars:4},
-    {img:"./onlineSeller Childpage/photos/childBag2.jpg",details:"yellow bag",stars:2.5},
+    {img:"./onlineSeller Childpage/photos/childDress4.jpg",setNew:true,NewDivStyle:"circle",details:"gril dress",stars:2},
+    {img:"./onlineSeller Childpage/photos/childSet3.jpg",setNew:true,NewDivStyle:"circle",details:"stagger gray black",stars:3.5},
+    {img:"./onlineSeller Childpage/photos/childSet4.jpg",setNew:true,NewDivStyle:"circle",details:"stagger all black",stars:7},
+    {img:"./onlineSeller Childpage/photos/childShoes3.jpg",setNew:true,NewDivStyle:"circle",details:"shoes army",stars:4},
+    {img:"./onlineSeller Childpage/photos/childBag4.jpg",setNew:true,NewDivStyle:"circle",details:"pink bag",stars:2.5},
+    {img:"./onlineSeller Childpage/photos/childShoes2.jpg",setNew:true,NewDivStyle:"circle",details:"red shoes",stars:4},
+    {img:"./onlineSeller Childpage/photos/childBag2.jpg",setNew:true,NewDivStyle:"circle",details:"yellow bag",stars:2.5},
 ]
 
+let products="";
+let product ;
+let productContent;
+let createTheNewDiv;
+let img;
+let details;
+let divStars;
+let link;
+        
 let leftBtn;
 let rightBtn;;
-let products="";
-
 let temp;
-function productsCreation(ProductList,containerID,idType,productsAnimation){
+
+// -------- create Elements for view --------
+function createElements(ele){
+    product=document.createElement("div");
+    productContent=document.createElement("div");
+    createTheNewDiv=(ele.setNew) ? document.createElement("div") : "";
+    img=document.createElement("img");
+    details=document.createElement("p");
+    divStars=document.createElement("div");
+    link=document.createElement("a");
+}
+// -------- add content for Elements --------
+function setContentForElement(ele,idTypeForPeople){
+    img.src=`${ele.img}`;
+    details.textContent=`${ele.details}`;
+    link.textContent=` More >>`;
+    link.href=(idTypeForPeople==="children")? "./onlineSeller Childpage/childpage.html" : (idTypeForPeople==="men")? "./onlineSeller Menpage/menpage.html" : "./veiled-fashion/index.html"
+    if(createTheNewDiv != "") createTheNewDiv.textContent=` New`;
+}
+// -------- add classes for style --------
+function addClassesForElement(ele,index,idTypeForPeople,productsAnimation){
+    product.classList.add("col-sm-3","product")
+    product.classList.add("col-sm-3","product", (index===0 && productsAnimation==="animation-right")? `${productsAnimation}` : ((index===3 && productsAnimation==="animation-left")? `${productsAnimation}`:"0"))
+    productContent.classList.add("card")
+    if(createTheNewDiv != "") createTheNewDiv.classList.add(`new-div-for-${idTypeForPeople}-${ele.NewDivStyle}`)
+    details.classList.add(`details-${idTypeForPeople}`);
+    link.classList.add("btn","btn-outline-info",`product-btn-color-${idTypeForPeople}`);
+}
+//------- create left and right buttons -------
+function create_left_right_Btns(idTypeForPeople){
+    leftBtn=document.createElement("button");
+    rightBtn=document.createElement("button");
+    leftBtn.addEventListener("click",change)
+    rightBtn.addEventListener("click",change)
+    leftBtn.id=`${idTypeForPeople}L`;
+    rightBtn.id=`${idTypeForPeople}R`;
+    leftBtn.textContent=`<`
+    rightBtn.textContent=`>`
+    leftBtn.classList.add("left-side-button",`${idTypeForPeople}ColorBtn`);
+    rightBtn.classList.add("right-side-button",`${idTypeForPeople}ColorBtn`);
+}
+//------------------------ productsCreation function -----------------------
+function productsCreation(ProductList,containerID,idTypeForPeople,productsAnimation){
         // get only frist products
         temp=[ProductList[0],ProductList[1],ProductList[2],ProductList[3]]
         temp.forEach(function (ele,index){
         // --------- get the container Div ----------
         products=document.getElementById(containerID); 
         // -------- create Elements for view --------
-        const product=document.createElement("div");
-        const productContent=document.createElement("div");
-        const theNewCircle=document.createElement("div");
-        const img=document.createElement("img");
-        const details=document.createElement("p");
-        const divStars=document.createElement("div");
-        const link=document.createElement("a");
+        createElements(ele);
         // -------- add content for Elements --------
-        img.src=`${ele.img}`;
-        details.textContent=`${ele.details}`;
-        link.textContent=` More >>`;
-        link.href=(idType==="children")? "./onlineSeller Childpage/childpage.html" : (idType==="men")? "./onlineSeller Menpage/menpage.html" : "./veiled-fashion/index.html"
-        theNewCircle.textContent=` New`;
+        setContentForElement(ele,idTypeForPeople);
         // -------- add classes for style --------
-        product.classList.add("col-sm-3","product")
-        product.classList.add("col-sm-3","product", (index===0 && productsAnimation==="animation-right")? `${productsAnimation}` : ((index===3 && productsAnimation==="animation-left")? `${productsAnimation}`:"0"))
-        productContent.classList.add("card")
-        theNewCircle.classList.add(`new-div-for-${idType}`)
-        details.classList.add(`details-${idType}`);
-        link.classList.add("btn","btn-outline-info",`product-btn-color-${idType}`);
+        addClassesForElement(ele,index,idTypeForPeople,productsAnimation);
         // -------- append child the element --------
         productContent.appendChild(img,details)
         productContent.appendChild(details)
@@ -91,28 +125,21 @@ function productsCreation(ProductList,containerID,idType,productsAnimation){
         }
         productContent.appendChild(divStars)
         productContent.appendChild(link)
-        productContent.appendChild(theNewCircle)
+        if(createTheNewDiv != "") productContent.appendChild(createTheNewDiv)
         product.appendChild(productContent)
         products.appendChild(product)
     })
     //------- create left and right buttons -------
-    leftBtn=document.createElement("button");
-    rightBtn=document.createElement("button");
-    leftBtn.addEventListener("click",change)
-    rightBtn.addEventListener("click",change)
-    leftBtn.id=`${idType}L`;
-    rightBtn.id=`${idType}R`;
-    leftBtn.textContent=`<`
-    rightBtn.textContent=`>`
-    leftBtn.classList.add("left-side-button",`${idType}ColorBtn`);
-    rightBtn.classList.add("right-side-button",`${idType}ColorBtn`);
+    create_left_right_Btns(idTypeForPeople)
     products.appendChild(leftBtn)
     products.appendChild(rightBtn)
 }
+
 // ------ calling the productsCreation() function to create the products inside each container ------
 productsCreation(menProductList,"menProducts","men");
 productsCreation(womenProductList,"womenProducts","women");
 productsCreation(childrenProductList,"childrenProducts","children");
+
 
 // -------- delete products --------
 function deleteProducts(containerID){
